0741156a500adadd00c26da190480572eb18d58e
Maxime GALISSAIRE <20190218@B15-03.local>
2019-11-14 15:55:49 +0100

    fix Majdi's correction adding

---
+++ Majdi s correction/main.js
@@ -0,0 +1,13 @@
+const { duplicate, transform } = require('./streambox')
+const { upperCase } = require('voca')
+
+if (process.argv.length !== 3) {
+  console.log('usage: node main.js <FILENAME>')
+  process.exit(1)
+}
+
+const fileName = process.argv[2]
+// duplicate(fileName)
+
+transform(fileName, /Chopin/g, upperCase)
+transform(fileName, /Chopin/g, upperCase, false)
